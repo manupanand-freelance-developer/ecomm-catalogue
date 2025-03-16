03d7f5539285eacb8b679f602bfaf5b2be235aa3
manupanand <manupanandindianinvestor@gmail.com>
2025-03-16 18:29:39 +0530

    githubactions-repo-commit

---
+++ run.sh
@@ -0,0 +1,18 @@
+# to make sure that environment variables are present before running image
+
+
+if [ -z "${MONGO}"]; then
+    echo Input MONGO is missing
+    exit 1
+fi
+
+
+if [ -z "${MONGO_URL}"]; then
+    echo Input MONGO_URL is missing
+    exit 1
+fi
+
+
+
+
+node  /app/server.js
